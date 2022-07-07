9d2440fc17b56482c6e41e4a21be2e9d0438b0fc
amatureApe <amatureApe@gmail.com>
2022-07-07 15:26:16 -0500

    add cloudFunctions

---
+++ cloudFunctions/updateActiveItems.js
@@ -0,0 +1,21 @@
+Moralis.Cloud.afterSave("ItemListed", async (request) => {
+  const confirmed = request.object.get("confirmed");
+  console.log(confirmed)
+  const logger = Moralis.Cloud.getLogger();
+  logger.info("Looking for confirmed Tx");
+  if (confirmed) {
+    logger.info("Found Item!");
+    const ActiveItem = Moralis.Object.extend("ActiveItem");
+
+    const activeItem = new ActiveItem();
+    activeItem.set("marketplaceAddress", request.object.get("address"));
+    activeItem.set("nftAddress", request.object.get("nftAddress"));
+    activeItem.set("price", request.object.get("price"));
+    activeItem.set("tokenId", request.object.get("tokenId"));
+    activeItem.set("seller", request.object.get("seller"));
+    logger.info(`Adding Address: ${request.object.get("address")}. TokenId: ${request.object.get("tokenId")}`);
+    logger.info("Saving...");
+    await activeItem.save();
+  }
+
+});
